6e86cf7e9d7e602ee154b5ef61a6a9002e34ec45
Ilija Eftimov <ileeftimov@gmail.com>
2018-12-17 01:16:55 +0100

    Modify Issues.CLI.process to print limited number of issues

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -20,10 +20,16 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
     |> sort_in_descending_order()
+    |> last(count)
+    |> Issues.Printer.paint(["number", "created_at", "title"])
+  end
+
+  def process({user, project}) do
+    process({user, project, @default_count})
   end
 
   def sort_in_descending_order(issues) do
@@ -31,6 +37,12 @@ defmodule Issues.CLI do
     |> Enum.sort(fn item1, item2 ->
         item1["created_at"] <= item2["created_at"]
       end)
+  end
+
+  defp last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse
   end
 
   defp decode_response({:ok, body}), do: body
